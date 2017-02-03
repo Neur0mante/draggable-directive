b6f282455f37ce49d05a248053158ba4c8fec8bf
Neuromantex <rupert735@hotmail.com>
2017-02-03 15:50:18 +0100

    Some cleanup and debouncing implemented

--- src/draggable.directive.ts
+++ src/draggable.directive.ts
@@ -19,12 +19,14 @@ export class DraggableDirective implements OnDestroy {
     private el: ElementRef, private renderer: Renderer
   ) {
   }
-  @HostListener('mouseup', ['$event'])
+
   onMouseup(event: MouseEvent) {
     this.Δx = 0;
     this.Δy = 0;
     this.dragging = false;
-    this.drag$.unsubscribe();
+    if (!this.drag$.closed) {
+      this.drag$.unsubscribe();
+    }
   }
 
   @HostListener('mousedown', ['$event'])
@@ -33,30 +35,27 @@ export class DraggableDirective implements OnDestroy {
     this.Δx = event.x - this.el.nativeElement.offsetLeft;
     this.Δy = event.y - this.el.nativeElement.offsetTop;
     this.dragging = true;
-    
     let end = Observable.fromEvent(document, "mouseup")
       .subscribe((ev: MouseEvent) => {
-        this.onMouseup(ev)
+        this.onMouseup(ev);
       });
+    // The skip allows to avoid staggering in case of a single click. It's not super-elegant cause we are still building and destroying the sub.
     this.drag$ = Observable.fromEvent(document, "mousemove")
+      .skip(1)
       .subscribe((ev: MouseEvent) => {
         this.doTranslation(ev.x, ev.y);
-      })
+      });
   }
 
-  //   @HostListener('mousemove', ['$event'])
-  //   onMousemove(event: MouseEvent) {
-  //      if (this.dragging) {    
-  //     console.log("HostListener");
-  //     console.log(event);
-  //     // this.doTranslation(event.x, event.y);
-  //   }
+  // @HostListener('mousemove', ['$event'])
+  // onMousemove(event: MouseEvent) {
   // }
 
   ngAfterViewInit() {
     try {
+
+      console.log(this.el.nativeElement.style.getPropertyValue("position"));
       if (this.mustBePosition.indexOf(this.el.nativeElement.style.position) === -1) {
-        console.log(this.el.nativeElement.style.getPropertyValue("position"));
         console.warn(this.el.nativeElement, 'Must be having position attribute set to ' + this.mustBePosition.join('|'));
         // this.el.nativeElement.style.position = "relative";
         // this.renderer.setElementAttribute(this.el.nativeElement, 'position', 'relative');
